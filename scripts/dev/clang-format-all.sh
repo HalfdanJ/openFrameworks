0780b404afbd01fe249043def1a888faa16d9224
Jonas Jongejan <jongejan@google.com>
2018-03-10 15:13:26 -0500

    travis and clang-format version

--- scripts/dev/clang-format-all.sh
+++ scripts/dev/clang-format-all.sh
@@ -4,8 +4,17 @@ cd $SCRIPT_DIR
 
 OF_ROOT="../.."
 
+if type clang-format-5.0 2> /dev/null ; then
+    CLANG_FORMAT=clang-format-5.0
+elif type clang-format 2> /dev/null ; then
+    CLANG_FORMAT=clang-format
+else 
+    echo "No appropriate clang-format found (expected clang-format-5.0, or clang-format)"
+    exit 1
+fi
+
 for DIRECTORY in "$OF_ROOT/libs/openFrameworks"
 do
     echo "Formatting code under $DIRECTORY/"
-    find "$DIRECTORY" \( -name '*.h' -or -name '*.m' -or -name '*.cpp' -or -name '*.mm' \) -print0 | xargs -0 clang-format -i
+    find "$DIRECTORY" \( -name '*.h' -or -name '*.m' -or -name '*.cpp' -or -name '*.mm' \) -print0 | xargs -0 ${CLANG_FORMAT} -i
 done
